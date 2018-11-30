b5c8f57f2b5917f1d03ab545cfbf61e7fdafa60a
missEnergy <everseput@hotmail.com>
2018-11-30 12:55:44 +0100

    leuk hoor

--- src/main/scala/com/xomnia/ActorWorkshop.scala
+++ src/main/scala/com/xomnia/ActorWorkshop.scala
@@ -9,15 +9,17 @@ object ActorWorkshop extends App {
 
   val system: ActorSystem = ActorSystem("actorWorkshop")
 
-  val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
+//  val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
+//
+//  printer ! Greeting("Hello world!")
+//  printer ! Ellissa(2)
+//
+//  val greeter: ActorRef = system.actorOf(Greeter.props("hi", printer), "greeterActor")
+//
+//  greeter ! PrintInternalMessage()
+//  greeter ! ReplaceGreet("Marilena")
+//  greeter ! PrintInternalMessage()
 
-  printer ! Greeting("Hello world!")
-  printer ! Ellissa(2)
-
-  val greeter: ActorRef = system.actorOf(Greeter.props("hi", printer), "greeterActor")
-
-  greeter ! PrintInternalMessage()
-  greeter ! ReplaceGreet("Marilena")
-  greeter ! PrintInternalMessage()
+  val sumCalculator: ActorRef = system.actorOf(SumCalculator.props(Array(2,4)), "example_sum")
 
 }
--- src/main/scala/com/xomnia/SumCalculator.scala
+++ src/main/scala/com/xomnia/SumCalculator.scala
@@ -1,6 +1,6 @@
 package com.xomnia
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 
 
 object SumCalculator {
@@ -12,8 +12,30 @@ object SumCalculator {
 class SumCalculator(numbers: Array[Int]) extends Actor with ActorLogging {
   import SumCalculator._
 
-  override def preStart() = ???
+  var partialResults = List[BigInt]()
 
-  override def receive: Receive = ???
+  override def preStart() = {
+    numbers.length match {
+      case 1 =>
+        context.parent ! Result(numbers(0))
+        context.stop(self)
+      case 2 =>
+        context.parent ! Result(numbers(0) + numbers(1))
+        context.stop(self)
+      case _ => {
+        context.actorOf(SumCalculator.props(numbers.take(2)), "left")
+        context.actorOf(SumCalculator.props(numbers.drop(2)), "right")
+      }
+    }
+
+  }
+
+  override def receive: Receive = {
+    case Result(result) =>
+      partialResults = result :: partialResults
+      if (partialResults.length == 2)
+        context.parent ! Result(partialResults(0) + partialResults(1))
+        context.stop(self)
+  }
 
 }
